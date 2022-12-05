5e39a575d88c4df92d0ef6ba0310c68dd06f29bd
Jared Stillwell <75730966+stillhub@users.noreply.github.com>
2022-12-05 16:05:21 +0000

    Uploaded Azure Last Login Report script

---
+++ AzureLastLoginReport/Users_LastloginReport.ps1
@@ -0,0 +1,16 @@
+﻿# Script that creates a report of last login times for Azure accounts.
+# Script created by Jared Stillwell
+# Version: 1.0
+
+$Cred = Get-Credential
+Connect-MsolService -Credential $Cred
+Connect-AzureAD -Credential $Cred
+
+$Users = Get-MsolUser -all
+$Headers = "DisplayName`tUserPrincipalName`tLicense`tLastLogon" >>C:\Temp\AzureLastloginUsers.txt
+ForEach ($User in $Users){
+    $UPN = $User.UserPrincipalName
+    $LoginTime = Get-AzureAdAuditSigninLogs -top 1 -filter "userprincipalname eq '$UPN'" | select CreatedDateTime
+    $NewLine = $User.DisplayName + "`t" + $User.UserPrincipalName + "`t" + $User.Licenses.AccountSkuId + "`t" + $LoginTime.CreatedDateTime
+    $NewLine >>C:\Temp\AzureLastloginUsers.txt
+}
